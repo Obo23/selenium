b709507ed7b7a4d3fa6156bed486cf527cc66ba2
Obo23 <juanalberto.gomez@bq.com>
2022-04-23 13:17:47 +0200

    Updates

--- general.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  relay: async function (delayInms) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(2);
-      }, delayInms);
-    });
-  },
-};
